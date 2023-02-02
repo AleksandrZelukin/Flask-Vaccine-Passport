0986a86be263a7825fb04cdc3881ed4f20c96bfb
Aleksandr <alexandr44@inbox.lv>
2023-02-02 22:05:33 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+# run.py
+
+from app import app
+
+if __name__ == '__main__':
+    app.run()
+
+
+# Python + Flask - Setting up your database and registering a user with SQLite
+#  https://youtu.be/gbTLL6htiPg
